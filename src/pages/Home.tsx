b0607a195d62bafd63e2b1dc67179eb952ca9062
hamsu12 <hamsj2413@smail.kongju.ac.kr>
2025-09-06 19:32:12 +0900

    feat/가족 책자 레이아웃 제작 및 홈레이아웃 아울렛 조정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,41 @@
 import Header from "../components/Header";
-import CustomCalendar from "../components/CustomCalendar";
 import FamilyMembersCard from "../components/home/FamilyMembersCard";
 import FamilyBookshelf from "../components/home/FamilyBookshelf";
-import TodaysQuestion from "../components/home/TodaysQuestion";
-import { sampleAppointments, familyMembers, familyBooks, questionAnswers } from "../data/homeMockData";
+import { familyMembers, familyBooks } from "../data/homeMockData";
 import { EFL } from "../assets/icons";
+import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const Home: React.FC = () => {
+export default function HomeLayout() {
     const username = localStorage.getItem("userInfo")?.split("|")[1] || "";
     const [isPopUpOpen, setPopUpOpen] = useState(true);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setPopUpOpen(false);
-        }, 5000);
-
-        return () => {
-            clearTimeout(timer);
-        }
-    })
+        const t = setTimeout(() => setPopUpOpen(false), 5000);
+        return () => clearTimeout(t);
+    }, []);
 
     return (
         <div className="relative w-screen h-screen max-w-[1440px] pt-20 m-auto flex items-center justify-center px-14 bg-[#EBEDF0]">
             <Header />
-            <main className="grid grid-cols-[360px_573px_360px] grid-rows-[750px] gap-4 p-10 w-full">
-                {/* 왼쪽 열 */}
+            {/* 왼쪽*/}
+            <main className="grid grid-cols-[360px_1fr] gap-4 p-10 w-full">
                 <div className="flex flex-col gap-4">
                     <FamilyMembersCard members={familyMembers} />
                     <FamilyBookshelf books={familyBooks} />
                 </div>
-                {/* 중앙 열 */}
-                <TodaysQuestion answers={questionAnswers} />
 
-                {/* 오른쪽 열 */}
-                <CustomCalendar appointments={sampleAppointments} />
 
+                <Outlet />
             </main>
-            <div style={{ display: isPopUpOpen ? "" : "none" }} className="absolute bottom-20 left-1/2 -translate-x-1/2 flex justify-center items-center gap-2 bg-[#ECF5F1] border border-primary-200 w-[173px] h-[48px] rounded-sm">
+
+            <div
+                style={{ display: isPopUpOpen ? "" : "none" }}
+                className="absolute bottom-20 left-1/2 -translate-x-1/2 flex justify-center items-center gap-2 bg-[#ECF5F1] border border-primary-200 w-[173px] h-[48px] rounded-sm"
+            >
                 <img src={EFL} alt="EverFlow_Character" />
                 <p>{username.slice(1)}님, 환영해요!</p>
             </div>
         </div>
     );
-};
-
-export default Home;
+}
