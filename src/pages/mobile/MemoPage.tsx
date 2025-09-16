e7f7e0f09ed5671030a0a5befe1a2243a056389a
hamsu12 <hamsj2413@smail.kongju.ac.kr>
2025-09-16 20:37:48 +0900

    feat/mobile memo 컴포넌트 제작

--- src/pages/mobile/MemoPage.tsx
+++ src/pages/mobile/MemoPage.tsx
@@ -2,6 +2,7 @@ import MobileNav from "../../components/mobile/MobileNav";
 import {useLocation} from "react-router-dom";
 import MobileHeader from "../../components/mobile/MobileHeader.tsx";
 import LargeBackButton from "../../components/mobile/LargeBackButton.tsx";
+import MobileMemo from "../../components/mobile/MobileMemo.tsx";
 
 export default function MemoPage() {
     const location = useLocation();
@@ -9,18 +10,38 @@ export default function MemoPage() {
 
     return (
         <div className="min-h-screen bg-back-color">
+
             {!isLarge &&
                 (
                     <div>
                         <MobileHeader />
+                        <div className="flex flex-col mx-auto w-full max-w-[430px]">
+                            <div className="px-4 pt-20">
+                                <h2 className="font-semibold text-[20px] py-3">가족 메모장</h2>
+                                <div>
+                                    <MobileMemo/>
+                                </div>
+
+                            </div>
+                        </div>
                         <MobileNav />
                     </div>
                 )}
             {isLarge &&
                 (
-                    <div className=" px-4 py-20">
+                    <div className="px-4 py-6">
                         <MobileHeader isLarge={true} />
-                        <LargeBackButton/>
+                        <div className="mt-15 mx-auto w-full max-w-[430px]">
+                            <LargeBackButton/>
+                            <div className="mt-10">
+                                <h2 className="font-semibold text-[25px] text-[#A4635C] py-3">가족 메모장</h2>
+                                <div>
+                                    <MobileMemo isLarge={true}/>
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
                 )}
         </div>
