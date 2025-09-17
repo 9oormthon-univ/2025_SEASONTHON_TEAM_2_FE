c91378f37e5d1066d6140509598041bc26838d9b
hamsu12 <hamsj2413@smail.kongju.ac.kr>
2025-09-17 15:23:14 +0900

    fix: 가족메모 border 색상 동일

--- src/components/FamilyMemo.tsx
+++ src/components/FamilyMemo.tsx
@@ -4,7 +4,7 @@ import { NoteIcon } from "../assets/icons/home"
 const FamilyMemo = () => {
     const navigate = useNavigate();
     return (
-        <div className="w-full h-[62px] bg-white rounded-2xl font-kccganpan text-2xl text-primary-300 border border-[#D3D3D3] cursor-pointer flex items-center p-5 gap-2 relative" onClick={() => navigate("/home/family/memo")}>
+        <div className="w-full h-[62px] bg-white rounded-2xl font-kccganpan text-2xl text-primary-300 border border-light-gray cursor-pointer flex items-center p-5 gap-2 relative" onClick={() => navigate("/home/family/memo")}>
             <img src={NoteIcon} className="size-[30px]" />
             <p>가족 메모장</p>
             <div className="text-primary-200 absolute right-8 text-3xl">&gt;</div>
